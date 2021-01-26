6c5aa41974632292730fe2f70507db7dfe9dfbf5
Doug Davidoff <douglass.davidoff@gmail.com>
2021-01-26 17:47:02 -0500

    Employee.js file

--- Employee.js
+++ Employee.js
@@ -1,4 +1,3 @@
-// TODO: Write code to define and export the Employee class
 class Employee {
     constructor(name, id, email) {
         this.name = name;
@@ -6,11 +5,22 @@ class Employee {
         this.email = email;
     }
 
-    getName(){
+    getName() {
         return this.name;
+    } 
+
+    getId() {
+        return this.id;
     }
 
-   
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return "Employee";
+    }
+
 }
 
 module.exports = Employee;
